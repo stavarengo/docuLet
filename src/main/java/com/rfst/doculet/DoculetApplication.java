9e9a45d0f922f16c41ffcd55dc25909e7bf1d5b2
stavarengo <rafael.stavarengo@mollie.com>
2024-01-12 15:27:10 +0100

    Spring Initializr

---
+++ src/main/java/com/rfst/doculet/DoculetApplication.java
@@ -0,0 +1,13 @@
+package com.rfst.doculet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoculetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoculetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rfst/doculet/DoculetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfst.doculet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoculetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
